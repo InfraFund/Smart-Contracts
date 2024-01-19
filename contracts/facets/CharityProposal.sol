4aaa22825288e5b1cd60e8ddd379ee04aee458c4
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-01-19 14:39:21 -0500

    fix: add more arguments

--- contracts/facets/CharityProposal.sol
+++ contracts/facets/CharityProposal.sol
@@ -1,21 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {LibInfraFundStorage} from "../libraries/LibInfraFundStorage.sol";
+import { LibInfraFundStorage } from "../libraries/LibInfraFundStorage.sol";
+import { ICharityProposal } from "../interfaces/ICharityProposal.sol";
 
-contract CharityProposal {
+contract  CharityProposal is ICharityProposal {
 
     event RegisterCharityProposal(string indexed _symbol, string indexed _hashProposal);
     event ModifyCharityProposal(string indexed _oldHashProposal, string indexed _newHashProposal);
-
 
     function registerCharityProposal(
         string memory _name,
         string memory _symbol,
         string memory _hashProposal,
-        uint256 _targetAmount,
+        address _GC,
+        uint256 _targetAmount, 
         uint256 _startTime,
-        uint256 _expireTime 
+        uint256 _duration 
         ) external {
             
         LibInfraFundStorage.InfraFundStorage storage infraStorage = LibInfraFundStorage.infraFundStorage();
@@ -26,10 +27,10 @@ contract CharityProposal {
             _name,
             _symbol,
             msg.sender,
-            address(0),
-            address(0),
+            address(0), 
+            _GC,
             _startTime,
-            _expireTime,
+            _duration,
             _targetAmount,
             false
         );
@@ -38,14 +39,28 @@ contract CharityProposal {
         emit RegisterCharityProposal(_symbol, _hashProposal);
     }
 
-    function modifyCharityProposal(string memory _oldHashProposal, string memory _newHashProposal) external {
+    function modifyCharityProposal(
+        string memory _oldHashProposal, 
+        string memory _newHashProposal,
+        address _GC,
+        uint256 _startTime,
+        uint256 _duration,
+        uint256 _targetAmount
+        ) external {
         LibInfraFundStorage.InfraFundStorage storage infraStorage = LibInfraFundStorage.infraFundStorage();
 
         require(infraStorage.verifiedClients[msg.sender], "Not White Listed");
         require(infraStorage.charityProjects[_oldHashProposal].proposer == msg.sender, "You are not proposer for this proposal");
         require(!infraStorage.charityProjects[_oldHashProposal].isVerified, "Your proposal already verified , cant change current proposal");
 
-        infraStorage.charityProjects[_newHashProposal] = infraStorage.charityProjects[_oldHashProposal];
+        LibInfraFundStorage.CharityProject memory tmpProject = infraStorage.charityProjects[_oldHashProposal];
+
+        tmpProject.startTime = _startTime;
+        tmpProject.duration = _duration;
+        tmpProject.targetAmount = _targetAmount;
+        tmpProject.GC = _GC;
+
+        infraStorage.charityProjects[_newHashProposal] = tmpProject;
         
         emit ModifyCharityProposal(_oldHashProposal, _newHashProposal);
     }
