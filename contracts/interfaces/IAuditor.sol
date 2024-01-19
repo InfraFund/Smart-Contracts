30941b2ac1d68b3b2c51dc4fac91a0852c4bacaa
amir-wyvern <amirhosein_wyvern@yahoo.com>
2024-01-19 12:34:16 -0500

    fix: remove event

--- contracts/interfaces/IAuditor.sol
+++ contracts/interfaces/IAuditor.sol
@@ -3,7 +3,5 @@ pragma solidity ^0.8.0;
 
 interface IAuditor {
 
-    event VerifyProposal(address indexed auditor, string indexed _hashProposal);
-
-    function verifyProposla(string memory _hashProposal) external;
+    function verifyProposal(string memory _hashProposal) external;
 }
